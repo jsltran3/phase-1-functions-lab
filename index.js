bd4a11c14633fa8c1dbba112566ba373721827f1
Joseph <jsltran@gmail.com>
2021-12-16 19:16:05 -0800

    commit tree 9ee63b97d1a3d4e1d2f88985a75191b0aee337b4

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const distanceFeet = 264; 
+const startPoint = 42; 
+
+function distanceFromHqInBlocks(bk) {
+    let numOfBlocks = startPoint - bk; 
+    return Math.abs(numOfBlocks);
+}
+
+function distanceFromHqInFeet (ft) {
+    return distanceFromHqInBlocks(ft) * distanceFeet; 
+}
+
+function distanceTravelledInFeet(st, ed) {
+    let blocksTravelled = ed - st; 
+    return Math.abs(blocksTravelled) * distanceFeet; 
+}
+
+function calculatesFarePrice(a, b) {
+    let afterFreeSample = Math.abs(distanceTravelledInFeet(a, b) - 400) 
+    if (Math.abs(distanceTravelledInFeet(a, b)) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(a, b) > 401 && distanceTravelledInFeet(a, b) <= 2000) {
+        //return Math.abs((distanceTravelledInFeet(a, b) - freeSample) * 0.02);
+        return Math.abs(afterFreeSample * 0.02);
+    } else if (distanceTravelledInFeet(a, b) > 2000 && distanceTravelledInFeet(a, b) < 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(a, b) > 2500) {
+        return 'cannot travel that far'
+    }
+}
